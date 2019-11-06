c80dd2625179fad62826c0c0b288aa5a7d73987c
Fabian Wetzel <fabsenet@googlemail.com>
2019-11-06 21:37:17 +0100

    added selected libs

--- windows2mqtt/App.xaml.cs
+++ windows2mqtt/App.xaml.cs
@@ -13,5 +13,9 @@ namespace windows2mqtt
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //todo nlog, autofac, mqttnet
+        }
     }
 }
